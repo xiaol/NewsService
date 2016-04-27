ed6466ed9b6881ba536724c1b01fb706858ab6b2
Sun Laite <sunlt@lieying.com>
2016-04-27 15:40:39 +0800

    添加数据任务状态字段

--- models/appitem.py
+++ models/appitem.py
@@ -11,8 +11,8 @@ class AppItem(object):
     content = None  # 内容 ['img': '', 'text': '', 'video': '',...]
     app_name = None # 抓取源APP的名称
     app_icon = None # 抓取源APP的icon
-    status = 1 # 状态标识 1:正常;2:时间大于当前+24H;3:没有标题
-    task_status = 0 # 后续处理状态 0:未处理; 1:已上传; 2:数据异常; 3:上传失败
+    status = None # 状态标识 1:正常;2:时间大于当前+24H;3:没有标题
+    task_status = None # 后续处理状态 0:未处理; 1:已上传; 2:数据异常; 3:上传失败
     author = None
     content_html = None  # 文章原始内容
 
@@ -52,8 +52,11 @@ class AppItem(object):
         self.publish_time = str_from_timestamp(int(param_dict['published_date'])/1000)
         if int(param_dict['published_date'])/1000 > time.time() + 24*60*60:
             self.status = 2
+        else:
+            self.status = 1
         self.content_html = param_dict['detail_html']
-        self.insert_time =str_from_timestamp(time.time())
+        self.insert_time = str_from_timestamp(time.time())
+        self.task_status = 0
         return self
 
 
